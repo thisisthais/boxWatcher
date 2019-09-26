01b9d9157291bd33e6ffbf1b403d4ae1b6070d9b
Thais Laney <laney.thais@gmail.com>
2019-09-26 12:50:21 -0400

    naive bounce

--- js/box.js
+++ js/box.js
@@ -7,9 +7,23 @@ var averageEyeX;
 var averageEyeY;
 var boundary;
 var cubeTranslation = {
-  vecX: 0,
+  vecX: 1,
   vecY: 1
 };
+var last5EyeChecks;
+
+class EvictingList {
+  constructor(n) {
+    this.list = [];
+    this.maxSize = n;
+    this.currIndex = 0;
+  }
+
+  add(elem) {
+    this.list[this.currIndex] = elem;
+    this.currIndex = (this.currIndex + 1) % this.maxSize;
+  }
+}
 
 function updateMovingAverageEyeCoords(data) {
   if (!data) return;
@@ -24,27 +38,32 @@ function updateMovingAverageEyeCoords(data) {
   averageEyeX = (4 / 5) * eyeX + averageEyeX / 5;
   averageEyeY = (4 / 5) * eyeY + averageEyeY / 5;
 
+  last5EyeChecks.add([averageEyeX, averageEyeY]);
+
   document.getElementById('xPos').textContent = averageEyeX.toFixed(2);
   document.getElementById('yPos').textContent = averageEyeY.toFixed(2);
+}
+
+function isWithinErrorRange(eyePoint, min, max, error) {
+  return eyePoint >= min - error && eyePoint <= max + error;
 }
 
 function isLookingAtCube(data) {
   if (!data) return false;
   const visibleHeight = visibleHeightAtZDepth(0, camera);
   const visibleWidth = visibleWidthAtZDepth(0, camera);
-  const heightPrecisionDelta = 0.15 * visibleHeight;
-  const widthPrecisionDelta = 0.15 * visibleWidth;
-  // returning undefined for some reason
-  // const boundingBox = cube.computeBoundingBox();
-  // console.log(geometry, boundingBox);
-  const isXWithinDelta =
-    averageEyeX >= -0.25 - widthPrecisionDelta &&
-    averageEyeX <= 0.25 + widthPrecisionDelta;
-  const isYWithinDelta =
-    averageEyeY >= -0.25 - heightPrecisionDelta &&
-    averageEyeY <= 0.25 + heightPrecisionDelta;
+  const heightPrecisionDelta = 0.2 * visibleHeight;
+  const widthPrecisionDelta = 0.2 * visibleWidth;
 
-  return isXWithinDelta && isYWithinDelta;
+  boundary.geometry.computeBoundingBox();
+  const { min, max } = boundary.geometry.boundingBox;
+
+  return last5EyeChecks.list.some(point => {
+    return (
+      isWithinErrorRange(point[0], min.x, max.x, widthPrecisionDelta) &&
+      isWithinErrorRange(point[1], min.y, max.y, heightPrecisionDelta)
+    );
+  });
 }
 
 function cubeIsWithinCanvas() {
@@ -65,6 +84,27 @@ function cubeIsWithinCanvas() {
   return isWithinCanvas;
 }
 
+function detectEdgeCollision() {
+  const [sceneHeight, sceneWidth] = [
+    visibleHeightAtZDepth(0, camera),
+    visibleWidthAtZDepth(0, camera)
+  ];
+
+  boundary.geometry.computeBoundingBox();
+  const { min, max } = boundary.geometry.boundingBox;
+
+  if (min.x - 0.3 <= -sceneWidth / 2 || max.x + 0.3 >= sceneWidth / 2) {
+    return 'horizontal';
+  } else if (
+    min.y - 0.25 <= -sceneHeight / 2 ||
+    max.y + 0.25 >= sceneHeight / 2
+  ) {
+    return 'vertical';
+  }
+
+  return 'within';
+}
+
 function animate(data, clock) {
   // correct data only shows up after click training
   if (data != null && data.x) {
@@ -82,14 +122,19 @@ function animate(data, clock) {
     // cube.rotation.y += 0.01;
     // cube.rotation.x += 0.01;
 
-    if (cubeIsWithinCanvas()) {
-      const { vecX, vecY } = cubeTranslation;
-      cube.translateOnAxis(new THREE.Vector3(vecX, vecY, 0).normalize(), 0.1);
-    } else {
+    const collision = detectEdgeCollision();
+
+    if (collision == 'horizontal') {
       cubeTranslation.vecX *= -1;
+      const { vecX, vecY } = cubeTranslation;
+      cube.translateOnAxis(new THREE.Vector3(vecX, vecY, 0).normalize(), 0.03);
+    } else if (collision == 'vertical') {
       cubeTranslation.vecY *= -1;
       const { vecX, vecY } = cubeTranslation;
-      cube.translateOnAxis(new THREE.Vector3(vecX, vecY, 0).normalize(), 0.1);
+      cube.translateOnAxis(new THREE.Vector3(vecX, vecY, 0).normalize(), 0.03);
+    } else {
+      const { vecX, vecY } = cubeTranslation;
+      cube.translateOnAxis(new THREE.Vector3(vecX, vecY, 0).normalize(), 0.03);
     }
 
     boundary.update();
@@ -107,6 +152,7 @@ function animate(data, clock) {
 function init(data, clock) {
   canvas = document.querySelector('#myCanvas');
   renderer = new THREE.WebGLRenderer({ canvas });
+  last5EyeChecks = new EvictingList(5);
 
   const fov = 75;
   const aspect = 2; // the canvas default
